2f7e3bab418583071347814db1aa0fa110ec2287
carlosarriagacm <carlosarriagacm@gmail.com>
2017-09-23 17:47:29 -0500

    idol3: WLAN stuff

--- product/qcom-wlan.mk
+++ product/qcom-wlan.mk
@@ -9,7 +9,10 @@ PRODUCT_PACKAGES += \
     libqsap_sdk \
     libQWiFiSoftApCfg \
     libwcnss_qmi \
-    wcnss_service
+    wcnss_service \
+    libwpa_client \
+    wificond \
+    wifilogd
 
 PRODUCT_PACKAGES += \
     hostapd \
